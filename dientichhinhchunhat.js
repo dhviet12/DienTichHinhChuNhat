b9c45841fe427f27ed00493942bf31ed317edaef
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2020-11-09 21:17:28 +0700

    update tinh dien tich hinh chu nhat

---
+++ dientichhinhchunhat.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều dài");
+inputHeight = prompt("Nhập chiều rộng");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích hình chữ nhật là " + area);
